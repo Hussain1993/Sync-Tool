6a73371c0bd677d12897221df16375d6e1582e78
Hussain Miah <hmiah.1993@gmail.com>
2014-03-01 22:02:16 +0000

    Added dependencies and the logging config

---
+++ src/main/java/com/hussain/sync/main/MainApp.java
@@ -0,0 +1,12 @@
+package com.hussain.sync.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainApp {
+	public static void main(String [] args){
+		Logger LOG = LoggerFactory.getLogger(MainApp.class);
+		LOG.info("This is some message");
+	}
+
+}
